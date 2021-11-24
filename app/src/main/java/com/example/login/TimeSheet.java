57f2c87e149817f111eacc8de669a1615aeb14ee
Krishna <vi.user.statistics@gmail.com>
2021-11-24 22:35:36 +0530

    Design

--- app/src/main/java/com/example/login/TimeSheet.java
+++ app/src/main/java/com/example/login/TimeSheet.java
@@ -2,15 +2,13 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class TimeSheet extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        startActivity(new Intent(getApplicationContext(), TimeSheet.class));
+        setContentView(R.layout.activity_time_sheet);
     }
 }
